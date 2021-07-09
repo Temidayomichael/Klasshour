8758cf1836bbaa0bcfecf894915ce09daaf5476b
Dennis <dennischarity2019@gmail.com>
2021-07-09 23:08:38 +0100

    contribution to faq page

--- frontend/pages/faq.js
+++ frontend/pages/faq.js
@@ -1,16 +1,187 @@
 import React from 'react'
 import Head from 'next/head'
-import { Box } from "@chakra-ui/react"
+import { Box, Heading, Text, Accordion, AccordionItem, AccordionIcon, AccordionButton, AccordionPanel, Flex, Icon } from "@chakra-ui/react"
+import { RiArrowDownLine } from 'react-icons/ri'
+import { AiFillMinusCircle } from 'react-icons/ai'
 export default function request() {
     return (
         <>
-             <Head>
+            <Head>
                 <title>FAQ | Klasshour</title>
                 <link rel="icon" href="../img/home_logo.png" />
             </Head>
-                <Box bg="tomato" w="100%" p={4} color="white">
-                      FAQ
+            <Box className="mycontainer" m="auto" my="90" className="content">
+                <Box mb="10">
+                    <Box>
+                        <Heading as="h1" size="lg" isTruncated>
+                            Frequently Asked Questions
+                        </Heading>
+                        <Text fontSize="lg">We are here to help, see some of our customer FAQs below.</Text>
+                        <Accordion allowToggle mt="10">
+                            <AccordionItem>
+                                
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                       How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                               <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+
+                                            <Text pl='10'>
+                                                You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+                               
+                            </AccordionItem>
+                            <AccordionItem>
+                                
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                    How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+                                            
+
+                                            <Text pl='10'>
+                                            You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+
+                               
+                            </AccordionItem>
+                            <AccordionItem>
+                               
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                    How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+                                           
+                                            <Text pl='10'>
+                                            You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+                                 
+                            </AccordionItem>
+                            <AccordionItem>
+                               
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                    How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+                                            
+                                            <Text pl='10'>
+                                            You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+                                   
+                            </AccordionItem>
+                            <AccordionItem>
+                               
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                    How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+                                            
+                                            <Text pl='10'>
+                                            You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+                                   
+                            </AccordionItem>
+                            <AccordionItem>
+                                
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                    How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+                                           
+                                            <Text pl='10'>
+                                            You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+                                   
+                            </AccordionItem>
+                            <AccordionItem>
+                                
+                                        <h2>
+                                            <AccordionButton _focus={{
+                                                border: "0"
+                                            }}>
+                                                <Box w="90%" d='flex' >
+                                                    <Text pl='10' fontSize='lg' fontWeight='medium'>
+                                                    How do i signup to Klasshour?
+                                                    </Text>
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel d='flex' alignItems='center' pb={4} >
+                                           
+                                            <Text pl='10'>
+                                            You can signup to Klasshour by clicking on the register button.
+                                            </Text>
+                                        </AccordionPanel>
+                                   
+                            </AccordionItem>
+                        </Accordion>
+
+                    </Box>
+
                 </Box>
-            </>
+            </Box>
+        </>
     )
 }
